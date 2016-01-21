6e2683c65fe5551065d0b882c51254cd0ba6e922
Mike Sampson <mike@sambodata.com>
2016-01-21 23:44:54 +1100

    Start of CPU struct.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+// The 4004 is a 4 bit data / 12 bit address CPU therefore it doesn't really
+// fit into the standard integer types. Comments below show actual size of the
+// registers. Not sure how to best handle this.
+
+#[derive(Default, Debug)]
+struct CPU {                   // actual register size
+    accumulator: u8,           // u4
+    carry: bool,               // u1
+
+    program_counter: u16,      // u12
+
+    program_counter_1: u16,    // u12
+    program_counter_2: u16,    // u12
+    program_counter_3: u16,    // u12
+
+    index_registers: [u8; 16], // u12
+}
+
+
 fn main() {
-    println!("TODO: Write emulator.");
+    let cpu: CPU = Default::default();
+
+    println!("{:?}", cpu);
 }
