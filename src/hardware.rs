2375897637fd47d637cb697ae2cebfb5d52b3c7d
Mike Sampson <mike@sambodata.com>
2016-01-24 16:01:55 +1100

    Add rom access functions.

--- src/hardware.rs
+++ src/hardware.rs
@@ -16,4 +16,12 @@ impl Hardware {
             rom: [0; 4096]
         }
     }
+
+    pub fn rom_read_byte(&self, address: u16) -> u8 {
+        self.rom[address as usize] & 0b1111 // shouldn't need to mask here
+    }
+
+    pub fn rom_write_byte(&mut self, address: u16, value: u8) {
+        self.rom[address as usize] = value & 0b1111; // shouldn't need to mask here
+    }
 }
