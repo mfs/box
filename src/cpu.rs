8c169b8188335ffed56f8de1907ae376fba341af
Mike Sampson <mike@sambodata.com>
2016-01-28 22:51:27 +1100

    Switch to a more compact output format.
    We are not dumping ROM anymore, might add back in later. Should test
    this. A rom that stores an increasing value in each index register
    maybe.

--- src/cpu.rs
+++ src/cpu.rs
@@ -39,7 +39,7 @@ impl CPU {
 
     pub fn run(&mut self) {
         loop {
-            println!("{:#?}", self);
+            println!("{}", self);
             self.run_instruction();
             sleep(Duration::from_millis(10));
         }
@@ -117,9 +117,24 @@ impl CPU {
 
 impl fmt::Display for CPU {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "acc: {} carry: {} pc: {} pc1: {} pc2: {} pc3: {}\nir: {:?}",
+        write!(f, "acc: {:x} carry: {} pc: {:03x} pc1: {:03x} pc2: {:03x} pc3: {:03x}\n",
                self.accumulator, self.carry, self.program_counter,
                self.program_counter_1, self.program_counter_2,
-               self.program_counter_3, self.index_registers)
+               self.program_counter_3);
+
+        // tidy this up later
+        write!(f, "r{:02}: {:x} r{:02}: {:x}   r{:02}: {:x} r{:02}: {:x}   r{:02}: {:x} r{:02}: {:x}   r{:02}: {:x} r{:02}: {:x}\n",
+               0, self.index_registers[0], 1, self.index_registers[1],
+               2, self.index_registers[2], 3, self.index_registers[3],
+               4, self.index_registers[4], 5, self.index_registers[5],
+               6, self.index_registers[6], 7, self.index_registers[7]);
+
+        write!(f, "r{:02}: {:x} r{:02}: {:x}   r{:02}: {:x} r{:02}: {:x}   r{:02}: {:x} r{:02}: {:x}   r{:02}: {:x} r{:02}: {:x}\n",
+               8, self.index_registers[8], 9, self.index_registers[9],
+               10, self.index_registers[10], 11, self.index_registers[11],
+               12, self.index_registers[12], 13, self.index_registers[13],
+               14, self.index_registers[14], 15, self.index_registers[15]);
+
+        Ok(()) // wrong, fix properly.
     }
 }
